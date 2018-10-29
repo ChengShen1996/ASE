875d7423fc9fcf4f477fe49d72fb82b3020124f9
jeremy0jiang <jeremy0jiang@gmail.com>
2018-10-29 19:29:54 -0400

    add 1=1 test

---
+++ Hotel/src/test/java/checkInTest.java
@@ -0,0 +1,24 @@
+/**
+ * Created by jeremyjiang on 10/29/18.
+ */
+import org.junit.jupiter.api.Test;
+
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testfx.assertions.api.Assertions.assertThat;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import com.jfoenix.controls.JFXButton;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+public class checkInTest {
+
+
+    @Test
+    public void button() {
+
+        assertEquals(1,1);
+    }
+}
