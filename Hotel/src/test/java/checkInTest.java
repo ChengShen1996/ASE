c79969ad922b1c601a76c09bd3a29279c4b55324
jeremy0jiang <jeremy0jiang@gmail.com>
2018-10-29 20:47:26 -0400

    start writing tests

--- Hotel/src/test/java/checkInTest.java
+++ Hotel/src/test/java/checkInTest.java
@@ -1,6 +1,9 @@
 /**
  * Created by jeremyjiang on 10/29/18.
  */
+import database.DatabaseHandler;
+import javafx.scene.control.Alert;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import org.testfx.framework.junit5.ApplicationTest;
@@ -12,13 +15,40 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import com.jfoenix.controls.JFXButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
 public class checkInTest {
 
+    DatabaseHandler databaseHandler;
+    @Before
+    public void setup(){
+
+        databaseHandler = new DatabaseHandler();
+
+    }
+
 
     @Test
-    public void button() {
+    public void testInsert() {
+        String name = "name1";
+        String roomId = "1";
+        String checkInDate = "2012-12-12";
+        String checkOutDate = "2012-12-13";
+        String requirement = "burger";
 
-        assertEquals(2,2;
+
+        String qu = "INSERT INTO CUSTOMER VALUES("
+                + "'" + name + "',"
+                + roomId + ","
+                + "'" + checkInDate + "',"
+                + "'" + checkOutDate + "',"
+                + "'" + requirement + "',"
+                + "'" + "false" + "'"
+                + ")";
+
+        assertEquals(databaseHandler.execAction(qu),true);
     }
 }
