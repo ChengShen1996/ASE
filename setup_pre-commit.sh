d1826522bdfad1a241474ad8b9ff07e0022898dc
jeremy0jiang <jeremy0jiang@gmail.com>
2018-10-30 13:50:32 -0400

    right

--- setup_pre-commit.sh
+++ setup_pre-commit.sh
@@ -2,4 +2,5 @@
 git config --add checkstyle.jar  ./pre-commit/checkstyle-8.14-all.jar
 git config --add checkstyle.checkfile  ./pre-commit/google_checks.xml
 cp ./pre-commit/pre-commit .git/hooks/
+touch ./pre-commit/pre-commit
 echo "pre-commit set up success!"
