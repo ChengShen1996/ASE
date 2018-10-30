0d9e4cfff758768fd77710c3a22a27f5b91abea1
jeremy0jiang <jeremy0jiang@gmail.com>
2018-10-30 13:08:23 -0400

    changed a space

--- Hotel/src/main/java/CheckIn/HotelAssistant.java
+++ Hotel/src/main/java/CheckIn/HotelAssistant.java
@@ -17,8 +17,7 @@ public class HotelAssistant extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
-
+    
     public static void main(String[] args) {
         launch(args);
     }
