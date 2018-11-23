f8c300b2fde0e975466cb8c9a29454cbf2dde5d5
ChengShen1996 <617984532@qq.com>
2018-11-23 13:55:20 -0500

    Cheng add Manager revenue part database

--- Hotel/src/main/java/ManagerRevenue/ShowRevenueController.java
+++ Hotel/src/main/java/ManagerRevenue/ShowRevenueController.java
@@ -1,41 +1,33 @@
 package ManagerRevenue;
 
+import CheckIn.addGuestController;
 import database.DatabaseHandler;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import javafx.scene.control.*;
 
-import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javafx.scene.control.TextField;
 import javafx.fxml.Initializable;
-import java.sql.SQLException;
 
-import com.jfoenix.controls.JFXTextField;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ComboBox;
 
 public class ShowRevenueController implements Initializable{
+    DatabaseHandler databaseHandler;
+
+    public void initialize(URL url, ResourceBundle rb){
+        databaseHandler = new DatabaseHandler();
+        month.getItems().addAll("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
+        year.getItems().addAll("2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019");
+    }
 
     @FXML
-    private TableColumn<?, ?> revenue;
-
-    @FXML
-    private TextField total_revenue;
-
-    @FXML
-    private TableColumn<?, ?> revenue_date;
+    private TextField revenue;
 
     @FXML
     private ComboBox<String> month;
@@ -43,16 +35,54 @@ public class ShowRevenueController implements Initializable{
     @FXML
     private ComboBox<String> year;
 
-    DatabaseHandler databaseHandler;
+    @FXML
+    void show_revenue(ActionEvent event) {
+        String selectedMonth = month.getValue();
+        String selectedYear = year.getValue();
+        System.out.println(selectedMonth);
+        System.out.println(selectedYear);
+        if(selectedMonth == null){
+            String startDate = selectedYear + "-01-01";
+            String endDate = selectedYear+"-12-31";
+            String qu = "SELECT SUM(c.totalPrice) AS Total FROM CUSTOMER c WHERE c.checkOutDate BETWEEN  '"+ startDate +"'  AND  '"+ endDate +"' ";
+            ResultSet rs = databaseHandler.execQuery(qu);
+            try{
+                while(rs.next()){
+                    int totalRevenue = rs.getInt("Total");
+                    revenue.setText(Integer.toString(totalRevenue));
+                }
+            }catch (SQLException ex) {
+                Logger.getLogger(addGuestController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    else{
+        String startDate = selectedYear + "-" + selectedMonth + "-01";
+        String endDate = "";
+        if (selectedMonth == "01" || selectedMonth == "03" || selectedMonth == "05" || selectedMonth == "07" || selectedMonth == "08" || selectedMonth == "10" || selectedMonth == "12") {
+            endDate = selectedYear + "-" + selectedMonth + "-31";
+        } else if (selectedMonth == "02") {
+            endDate = selectedYear + "-" + selectedMonth + "-28";
+        } else {
+            endDate = selectedYear + "-" + selectedMonth + "-31";
+        }
 
-    public void initialize(URL url, ResourceBundle rb){
-        databaseHandler = new DatabaseHandler();
-        year.getItems().addAll("2012", "2013", "2014", "2015", "2016", "2017", "2018");
-        month.getItems().addAll("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
+        String qu = "SELECT SUM(c.totalPrice) AS Total FROM CUSTOMER c WHERE c.checkOutDate BETWEEN  '"+ startDate +"'  AND  '"+ endDate +"' ";
+        ResultSet rs = databaseHandler.execQuery(qu);
+        try {
+            while (rs.next()) {
+                int totalRevenue = rs.getInt("Total");
+                revenue.setText(Integer.toString(totalRevenue));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(addGuestController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+
     }
 
     @FXML
-    void show_revenue(ActionEvent event) {
+    void reset_all(ActionEvent event) {
 
     }
 }
